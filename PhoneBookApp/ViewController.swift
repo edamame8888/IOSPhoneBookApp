4599ba9664e7c0b5a539b7f0a28331e20bbaee12
今枝弘貴 <imaeda.k@amakawawainomacbook-puro.local>
2018-09-13 15:41:58 +0900

    Initial Commit

---
+++ PhoneBookApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhoneBookApp
+//
+//  Created by 今枝弘貴 on 2018/09/13.
+//  Copyright © 2018年 K.Imaeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
